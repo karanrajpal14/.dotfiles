67c1bfbf05d327ebf5d499b2ed257d634baa7cb4
Karan M Rajpal <rajpal.karan73@gmail.com>
2018-01-11 15:12:51 +0530

    Ensuring installation of Java 8 prior to Studio

--- studio.sh
+++ studio.sh
@@ -1,4 +1,11 @@
 apt install software-properties-common python-software-properties -y
+add-apt-repository ppa:webupd8team/java
+dpkg --configure -a
+apt-get update
+apt-get install java-common oracle-java8-installer -y
+# apt-get install oracle-java8-set-default -y
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 apt-add-repository ppa:maarten-fonville/android-studio -y
+dpkg --configure -a
 apt update
 apt-get install android-studio -y
