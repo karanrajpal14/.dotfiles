a4827dc58975defc13a04e9bd7e5b1158bb6a724
Karan M Rajpal <rajpal.karan73@gmail.com>
2018-01-11 16:29:56 +0530

    Installing Java 8 before Android Studio

--- base-install.sh
+++ base-install.sh
@@ -42,9 +42,10 @@ read -p "Do you want to install Android Studio? [y/n] " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+	add-apt-repository ppa:webupd8team/java -y
 	apt-add-repository ppa:maarten-fonville/android-studio -y
-	apt update
-	apt-get install android-studio -y
+	apt-get update
+	apt-get install java-common oracle-java8-installer oracle-java8-set-default android-studio -y
 fi
 
 ### Sublime Text ###
