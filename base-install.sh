8027dd6e3718cc161a7fb047e3634f37b75a757f
Karan M Rajpal <rajpal.karan73@gmail.com>
2018-01-12 10:45:00 +0530

    Removed redunadant sudo commands for ST3

--- base-install.sh
+++ base-install.sh
@@ -54,9 +54,9 @@ read -p "Do you want to install Sublime Text 3? [y/n] " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-	apt-get update
+	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
+	echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
+	apt update
 	apt-get install sublime-text -y
 fi
 
